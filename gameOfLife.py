bb0a7550abcfa2be419dbb5d0ec8fccfc617a090
evan brittain <ebrittain916@gmail.com>
2019-05-26 10:02:30 -0700

    add scripts

---
+++ gameOfLife.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+N = 100
+ON = 255
+OFF = 0
+updateInterval = 50
+
+def update(frameNum, img, grid, N):
+    newGrid = grid.copy()
+
+    for i in range(N):
+        for j in range(N):
+            total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] +
+                         grid[(i-1)%N, j] + grid[(i+1)%N, j] +
+                         grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
+                         grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N]) / 255)
+            if grid[i, j] == ON:
+                if (total < 2) or (total > 3):
+                    newGrid[i, j] = OFF
+            else:
+                if total == 3:
+                    newGrid[i, j] = ON
+
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return img,
+
+def main():
+    grid = np.random.choice([ON, OFF], N*N, p=[0.2, 0.8]).reshape(N, N)
+
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
+                                frames=10,
+                                interval=updateInterval,
+                                save_count=50)
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
+
+
